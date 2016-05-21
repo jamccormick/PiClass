037e5557bd43dc52ffa5b9b16e1485b5b66ed498
jamccormick <james.a.mccormick@me.com>
2016-05-21 14:21:18 -0500

    Update temp.sh to include a direct call to the Python interpreter

--- temp.sh
+++ temp.sh
@@ -4,5 +4,5 @@ sudo modprobe w1-gpio
 sudo modprobe w1-therm
 
 device=$(ls /sys/bus/w1/devices | grep ^28.*$)
-sudo temp.py /sys/bus/w1/devices/$device
+sudo python3 temp.py /sys/bus/w1/devices/$device
 
